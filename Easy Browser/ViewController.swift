3869258ed0d6759f44aa5be935e1810d2ae9f398
murad <muradkv@gmail.com>
2019-04-21 23:14:31 +0300

    Show webView.title on title view that describes what url was open

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -44,5 +44,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
     }
 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
